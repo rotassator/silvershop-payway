6ee10fc9207084f87cb19c9bd4926393822ace66
Steve Dixon <steve@solutionsoutsourced.com.au>
2016-12-14 14:17:28 +1100

    FEAT: added framework for recurring payments

--- code/extensions/PaywayOrderExtension.php
+++ code/extensions/PaywayOrderExtension.php
@@ -6,6 +6,8 @@ class PaywayOrderExtension extends DataExtension
 {
     /** @var array List of database fields */
     private static $db = array(
-        'CustomerNumber' => 'Varchar(255)',
+        'CustomerNumber'   => 'Varchar(255)',
+        'PaymentFrequency' => "Enum('once, weekly, fortnightly, monthly, quarterly, six-monthly, yearly', 'once')",
+        'PaymentDateNext'  => 'Date',
     );
 }
