30b91870550632ccd91bdd1e03455ce26cbfe06e
Steve Dixon <steve@solutionsoutsourced.com.au>
2016-12-19 14:27:58 +1100

    FEAT: added direct debit payment gateway

--- code/checkout/steps/CheckoutStep_PaywayPayment.php
+++ code/checkout/steps/CheckoutStep_PaywayPayment.php
@@ -17,8 +17,7 @@ class CheckoutStep_PaywayPayment extends CheckoutStep
     public function paymentdetails()
     {
         // continue if not PayWay
-        $order = ShoppingCart::curr();
-        if (Checkout::get($order)->getSelectedPaymentMethod() !== 'PaywayRest') {
+        if (!$this->isPayway()) {
             $this->owner->redirect($this->owner->Link('payment'));
         }
 
@@ -26,7 +25,8 @@ class CheckoutStep_PaywayPayment extends CheckoutStep
         $this->owner->extend('updatePaymentDetailsForm', $form);
 
         return array(
-            'OrderForm' => $form,
+            'OrderForm'     => $form,
+            'PaymentMethod' => $this->getPaymentMethod(),
         );
     }
 
@@ -52,16 +52,41 @@ class CheckoutStep_PaywayPayment extends CheckoutStep
     }
 
     /**
+     * Get selected payment method
+     * @return string [description]
+     */
+    protected function getPaymentMethod()
+    {
+        // payment method is stored in the order
+        $order = ShoppingCart::curr();
+        return Checkout::get($order)->getSelectedPaymentMethod();
+    }
+
+    /**
+     * Check if a Payway payment method is selected
+     * @return boolean True if a Payway method
+     */
+    protected function isPayway()
+    {
+        return in_array($this->getPaymentMethod(), array(
+            'PaywayRest',
+            'PaywayRest_DirectDebit',
+        ));
+    }
+
+    /**
      * Get config array or parameter
      * @param  string       $parm Config parameter name
      * @return string|array       String if parameter; array if no parameter
      */
-    protected function getConfig($parm = null)
+    protected function getConfig($parm = null, $gateway = null)
     {
+        $gateway = $gateway ?: $this->getPaymentMethod();
+
         // get PayWay config
-        $config = Config::inst()->get('GatewayInfo', 'PaywayRest');
+        $config = Config::inst()->get('GatewayInfo', $gateway);
         if (!isset($config['parameters'])) {
-            user_error('Gateway parameters not found. Should be in GatewayInfo.Payway.parameters');
+            user_error("Gateway parameters not found. Should be in GatewayInfo.{$gateway}.parameters");
         }
         // check for specific parameter value
         if ($parm) {
@@ -78,19 +103,19 @@ class CheckoutStep_PaywayPayment extends CheckoutStep
     {
         // collect config so it can be extended
         $jsConfig = array(
-            'apiLinkCreditCardForm' => $this->getConfig('apiLinkCreditCardForm'),
-            'apiKeyPublic'  => $this->getConfig('apiKeyPublic'),
-            'returnUrl'     => Director::absoluteURL($this->owner->Link('payment')),
+            'apiLinkCreditCardForm' => $this->getConfig('apiLinkCreditCardForm', 'PaywayRest'),
+            'apiKeyPublic'          => $this->getConfig('apiKeyPublic', 'PaywayRest'),
+            'returnUrl'             => Director::absoluteURL($this->owner->Link('payment')),
         );
 
         $this->owner->extend('updatePaywayJSConfig', $jsConfig);
 
         // check the config
         if (empty($jsConfig['apiLinkCreditCardForm'])) {
-            user_error(_t(__CLASS__ . '.MissingApiLinkCCForm', 'Credit Card Form API link was not set - should be in GatewayInfo.Payway.parameters.apiLinkCreditCardForm'));
+            user_error(_t(__CLASS__ . '.MissingApiLinkCCForm', 'Credit Card Form API link was not set - should be in GatewayInfo.PaywayRest.parameters.apiLinkCreditCardForm'));
         }
         if (empty($jsConfig['apiKeyPublic'])) {
-            user_error(_t(__CLASS__ . '.MissingApiPublicKey', 'Publishable API key was not set - should be in GatewayInfo.Payway.parameters.apiKeyPublic'));
+            user_error(_t(__CLASS__ . '.MissingApiPublicKey', 'Publishable API key was not set - should be in GatewayInfo.PaywayRest.parameters.apiKeyPublic'));
         }
         if (empty($jsConfig['returnUrl'])) {
             user_error(_t(__CLASS__ . '.MissingReturnUrl', 'Return URL was not set - should be set to the Checkout payment link'));
@@ -99,6 +124,42 @@ class CheckoutStep_PaywayPayment extends CheckoutStep
         // return JS API link
         return join(array(
             $jsConfig['apiLinkCreditCardForm'],
+            '?apiKey=',
+            $jsConfig['apiKeyPublic'],
+            '&amp;redirectUrl=',
+            $jsConfig['returnUrl'],
+        ));
+    }
+
+    /**
+     * Get link to PayWay Bank Account processing form script
+     * @return string Link to form script
+     */
+    public function getPaywayBankAccountJSLink()
+    {
+        // collect config so it can be extended
+        $jsConfig = array(
+            'apiLinkBankAccountForm' => $this->getConfig('apiLinkBankAccountForm', 'PaywayRest_DirectDebit'),
+            'apiKeyPublic'           => $this->getConfig('apiKeyPublic', 'PaywayRest_DirectDebit'),
+            'returnUrl'              => Director::absoluteURL($this->owner->Link('payment')),
+        );
+
+        $this->owner->extend('updatePaywayBankAccountJSConfig', $jsConfig);
+
+        // check the config
+        if (empty($jsConfig['apiLinkBankAccountForm'])) {
+            user_error(_t(__CLASS__ . '.MissingApiLinkBAForm', 'Bank Account Form API link was not set - should be in GatewayInfo.PaywayRest_DirectDebit.parameters.apiLinkBankAccountForm'));
+        }
+        if (empty($jsConfig['apiKeyPublic'])) {
+            user_error(_t(__CLASS__ . '.MissingApiBAPublicKey', 'Publishable API key was not set - should be in GatewayInfo.PaywayRest_DirectDebit.parameters.apiKeyPublic'));
+        }
+        if (empty($jsConfig['returnUrl'])) {
+            user_error(_t(__CLASS__ . '.MissingReturnUrl', 'Return URL was not set - should be set to the Checkout payment link'));
+        }
+
+        // return JS API link
+        return join(array(
+            $jsConfig['apiLinkBankAccountForm'],
             '?apiKey=',
             $jsConfig['apiKeyPublic'],
             '&amp;redirectUrl=',
